473b6868c7eb6f982b2bb522a2cd49f09090edca
janardannn <janardonhazarika7@gmail.com>
2025-08-22 03:36:13 +0530

    vercel deploy fix

---
+++ src/app/api/courses/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// dummy export to pass build
+export async function GET() {
+    return NextResponse.json({ message: "Hello - GET" }, { status: 200 });
+}
